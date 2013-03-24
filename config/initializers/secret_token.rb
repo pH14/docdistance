22a8b5d4da7260f6909ea26cd4eddf79a4c8a307
Paul Hemberger <pH14@mit.edu>
2013-03-23 22:45:36 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Docdistance::Application.config.secret_token = '130357b6a6c7ae6ae94b1fea0227b72cd5eaa1c23e0a7ca71aa8259c5f7a1d3997694c57526e1cfdc623d4883d31d8c467256d2c0e944373186b540f6f46a13d'
